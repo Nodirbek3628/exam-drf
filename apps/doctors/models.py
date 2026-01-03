63414d5e568f6c646c0f358368a6f72ab6c00c5a
Nodirbek <ablokulovnodirbek4@gmail.com>
2026-01-03 23:04:16 +0500

    doctor modell

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -17,4 +17,4 @@ class DoctorProfile(models.Model):
     )
 
     def __str__(self):
-        return f"{self.user.username}({self.specialization})"
+        return f"{self.user.username} ({self.specialization})"
