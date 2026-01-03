90dd2ba1aa6fd9d25785995cbc47b031c27fcccf
Nodirbek <ablokulovnodirbek4@gmail.com>
2026-01-03 11:02:11 +0500

    users model

---
+++ apps/users/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import CustomUser,PatientProfile
+
+
+admin.site.register(CustomUser)
+admin.site.register(PatientProfile)
---
+++ apps/users/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        DOCTOR = 'doctor', 'Doctor'
+        PATIENT = 'patient', 'Patient'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.PATIENT
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
+
+    @property
+    def is_admin(self) -> bool:
+        return self.role == self.Role.ADMIN
+
+    @property
+    def is_doctor(self) -> bool:
+        return self.role == self.Role.DOCTOR
+
+    @property
+    def is_patient(self) -> bool:
+        return self.role == self.Role.PATIENT
+
+
+class PatientProfile(models.Model):
+
+    class Gender(models.TextChoices):
+        MALE = 'male', 'Male'
+        FEMALE = 'female', 'Female'
+
+    user = models.OneToOneField(
+        'users.CustomUser',
+        on_delete=models.CASCADE,
+        related_name='patient_profile'
+    )
+    phone = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username} (Patient)"
